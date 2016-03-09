e2f423c7c1c75a8bc5e1a790b493fbd0b8d80af8
raphael <raphael@raphael>
2016-03-09 15:47:55 +0100

    workspace

---
+++ workspace/projet_token/src/projet_token/testApplet.java
@@ -0,0 +1,18 @@
+package projet_token;
+
+
+import java.applet.*;
+
+
+public class testApplet extends Applet {
+	
+	public int test = 10;
+	
+	public int setTest(int val){
+		
+		test=val;
+		
+		return test;
+	}
+
+}
