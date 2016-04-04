8ca93e7a49c5faa384ab3f72781a0d46d8723045
valoulou <valentin@localhost.localdomain>
2016-04-04 14:08:11 +0200

    bonjour

---
+++ chrome_extension/test.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	var checkPageButton = document.getElementById('checkPage');
+	var application = document.getElementById('yolo');
+	var ha = document.getElementById('lpassword');
+
+	//var truc = application.setTest(12);
+
+	application.setString(ha.value);
+
+	//document.write(truc);
+
+	document.write("Bonjour");
+
+}, false);
