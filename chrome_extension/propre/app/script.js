472e3e26e5c15495fbf6d8a0aae8fa8f0f5a646a
valoulou <valentin@localhost.localdomain>
2016-05-09 17:36:24 +0200

    MAJ

--- chrome_extension/propre/app/script.js
+++ chrome_extension/propre/app/script.js
@@ -36,14 +36,14 @@ function send(laddress, leport, ledata){
     
     chrome.sockets.tcp.connect(MaSocket,
     laddress, parseInt(leport, 10), function(){
-    	var secureOption = {
+    	/*var secureOption = {
     		tlsVersion: {min: 'ssl3', max: 'tls1.2'}
-    	};
+    	};*/
     	
-    	chrome.sockets.tcp.secure(MaSocket, secureOption, function(){
+    	//chrome.sockets.tcp.secure(MaSocket, secureOption, function(){
 			chrome.sockets.tcp.send(MaSocket, str2ab(ledata), function(){
     		});
-    	});   
+    	//});   
   });
   });
 }
